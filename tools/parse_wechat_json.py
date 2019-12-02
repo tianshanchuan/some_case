e8528265ebc60a57052b4aed9e9e1ece125cb3e1
tianshanchuan <tianshanchuan@medlinker.com>
2019-12-02 14:53:50 +0800

    update field

--- tools/parse_wechat_json.py
+++ tools/parse_wechat_json.py
@@ -2,10 +2,10 @@ import json
 import os
 import csv
 
-goal_json_files = []
+path = r'/Users/tiansc/Downloads/20小升初社群/'
+out_path = r'/Users/tiansc/Downloads/20小升初社群/20小升初社群_ret.csv'
 
-path = r'/Users/tiansc/Downloads/新建文件夹/'
-out_path = r'/Users/tiansc/Downloads/新建文件夹/ret.csv'
+goal_json_files = []
 
 g = os.walk(path) 
 for path,dir_list,file_list in g:  
@@ -14,8 +14,9 @@ for path,dir_list,file_list in g:
 
 goal_json_files = [ x for x in goal_json_files if x[0].endswith('.json')]
 
-# with open(out_path, 'w+', encoding='utf8') as j:
-with open(out_path, 'w+') as j:
+# print(goal_json_files)
+
+with open(out_path, 'w+', encoding='utf-8') as j:
     writer = csv.writer(j)
     for file_path in goal_json_files[:]:
         with open(file_path[0],'rb') as f:
@@ -24,12 +25,14 @@ with open(out_path, 'w+') as j:
                 if item['nick_name']:
                     nick_name = item['nick_name']
                 else:
-                    nick_name = ''
+                    nick_name = None
                 try:
                     remark_name = item['remark_name']
                 except:
-                    remark_name = ''
-                kv_lst = [file_path[1],nick_name, remark_name]
-                kv_lst = json.dumps(kv_lst)
+                    remark_name = None
+                wxid = item['wxid']
+                user_name = item['user_name']
+                kv_lst = [file_path[1],nick_name,wxid, user_name,remark_name]
+                print(kv_lst)
                 writer.writerows([kv_lst])
-                print(file_path[1],nick_name, remark_name)
+#                 print(file_path[1],nick_name, remark_name)
